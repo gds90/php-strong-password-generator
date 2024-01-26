f08354459842e96f7da9614adb8b91a201c74850
gds90 <you@example.com>
2024-01-26 16:57:43 +0100

    Added bootstrap in my_password.php

--- my_password.php
+++ my_password.php
@@ -21,7 +21,8 @@ if ($lunghezza > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Password randomica</title>
 </head>
 
 <body>
